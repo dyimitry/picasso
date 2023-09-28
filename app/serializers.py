69449f6e8d0417695cc2f72fccb9a4078a0e552e
Дмитрий Шинкарев <dimonshinkariov111@yandex.ru>
2023-09-29 00:11:18 +0300

    change docker-compose.yaml

--- app/serializers.py
+++ app/serializers.py
@@ -3,6 +3,11 @@ from app.models import InputFile
 
 
 class InputFileSerializer(serializers.ModelSerializer):
+    file = serializers.SerializerMethodField()
+
     class Meta:
         model = InputFile
         fields = ('file', 'uploaded_at', 'processed')
+
+    def get_file(self, obj):
+        return obj.file.name
